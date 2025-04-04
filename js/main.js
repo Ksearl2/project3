5e86e3d6098a9e83572ee49cb1451b46fc4f1a43
Ksearl2 <ksearle1284@gmail.com>
2025-04-03 22:31:05 -0500

    copy

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 // JS scripts placed here
+
+$(document).ready(function(){
+    $('.landscape').slick({
+      autoplay: true,
+      slidesToShow: 1,
+      arrows: true,
+      autoplaySpeed: 1000,
+    });
+  });
